64633c2d1b5edfe0e8b6bc876b4d8df6a95ad03b
Miloš Zeković <milos.zekov@gmail.com>
2019-05-31 08:53:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// element.addEventListener('click', doSomething, false)
 let btn1 = document.querySelector(".button1"),
   btn2 = document.querySelector(".button2"),
   btn3 = document.querySelector(".button3"),
   btn4 = document.querySelector(".button4"),
   op = document.querySelector(".output")
 
+// element.addEventListener('click', doSomething, false)
 btn1.addEventListener("click", function() {
     op.innerHTML += "<br />" + "Kliknuto!";
   },
